3580e342d9d9715d3db23838e3853fc1557e72dd
Suman Karki <sumankarki16@gmail.com>
2024-02-03 02:56:49 +0545

    Smooth Nevigation

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+
+///////////////////////////////////////
+// Modal window
+
+
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -30,6 +37,57 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+
+
+//// Button Scrolling //////////////
+
+btnScrollTo.addEventListener('click',function(e){
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log('current scroll (X/Y)',window.scrollX,window.scrollY);
+
+
+  // window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY);
+/* 
+  window.scrollTo({
+    left: s1coords.left + window.scrollX, 
+    top: s1coords.top + window.scrollY,
+    behavior: 'smooth',
+  });
+ */
+
+  section1.scrollIntoView({behavior: 'smooth'});
+
+});
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+/////////// Page Navigation
+/* 
+document.querySelectorAll('.nav__link').forEach(function(el){
+  el.addEventListener('click',function(e){
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  });
+});
+ */
+
+document.querySelector('.nav__links').addEventListener('click',function(e){
+  e.preventDefault();
+
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+});
 
 ////////////////////////////////////////////////////
 /* 
@@ -68,29 +126,3 @@ message.style.backgroundColor = '#37383d';
 message.style.width = '105%';
  */
 
-//////////// Scroll To /////////////////
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click',function(e){
-  const s1coords = section1.getBoundingClientRect();
-  // console.log(s1coords);
-
-  // console.log(e.target.getBoundingClientRect());
-
-  // console.log('current scroll (X/Y)',window.scrollX,window.scrollY);
-
-
-  // window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY);
-/* 
-  window.scrollTo({
-    left: s1coords.left + window.scrollX, 
-    top: s1coords.top + window.scrollY,
-    behavior: 'smooth',
-  });
- */
-
-  section1.scrollIntoView({behavior: 'smooth'});
-
-});
