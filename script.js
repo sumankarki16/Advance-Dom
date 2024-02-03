f3bde5bbcc80fac804b2e597f1bbf5c73413bee9
Suman Karki <sumankarki16@gmail.com>
2024-02-04 01:15:36 +0545

    Adding Tabbed Component

--- script.js
+++ script.js
@@ -90,6 +90,33 @@ document.querySelector('.nav__links').addEventListener('click',function(e){
 });
 
 ////////////////////////////////////////////////////
+/////////// Tabbed Component
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click',function(e){
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause
+  if (!clicked) return;
+  
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tc => tc.classList.remove('operations__content--active'))
+  
+  //Active Tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate Content Area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+});
+
+
+
+////////////////////////////////////////////////////
 /* 
 console.log(document.documentElement);
 console.log(document.head);
